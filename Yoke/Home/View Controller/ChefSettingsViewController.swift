9a7ff82e48e71127bf5b73cf4f0710728b153a2d
LAURA JELENICH <ljelenich85@gmail.com>
2021-01-21 17:07:37 -0500

    code cleanup

--- Yoke/Home/View Controller/ChefSettingsViewController.swift
+++ Yoke/Home/View Controller/ChefSettingsViewController.swift
@@ -17,7 +17,7 @@ class ChefSettingsViewController: UIViewController, TTGTextTagCollectionViewDele
     var safeArea: UILayoutGuide {
         return self.view.safeAreaLayoutGuide
     }
-    let tagCollectionView = TTGTextTagCollectionView()
+    let cusineCollectionView = TTGTextTagCollectionView()
     private var selections = [String]()
     let uid = Auth.auth().currentUser?.uid ?? ""
     let firestoreDB = Firestore.firestore()
@@ -46,7 +46,7 @@ class ChefSettingsViewController: UIViewController, TTGTextTagCollectionViewDele
         view.addSubview(cusineTypeTextField)
         view.addSubview(addButton)
         view.addSubview(selectionLabel)
-        view.addSubview(tagCollectionView)
+        view.addSubview(cusineCollectionView)
     }
     
     func constrainViews() {
@@ -58,52 +58,31 @@ class ChefSettingsViewController: UIViewController, TTGTextTagCollectionViewDele
         cusineTypeTextField.anchor(top: chefLabel.bottomAnchor, left: safeArea.leftAnchor, bottom: nil, right: nil, paddingTop: 20, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, height: 45)
         addButton.anchor(top: chefLabel.bottomAnchor, left: cusineTypeTextField.rightAnchor, bottom: nil, right: safeArea.rightAnchor, paddingTop: 28, paddingLeft: 8, paddingBottom: 0, paddingRight: 8, width: 50, height: 30)
         selectionLabel.anchor(top: cusineTypeTextField.bottomAnchor, left: safeArea.leftAnchor, bottom: nil, right: nil, paddingTop: 20, paddingLeft: 8, paddingBottom: 0, paddingRight: 0)
-        tagCollectionView.anchor(top: selectionLabel.bottomAnchor, left: safeArea.leftAnchor, bottom: nil, right: safeArea.rightAnchor, paddingTop: 5, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, height: 200)
+        cusineCollectionView.anchor(top: selectionLabel.bottomAnchor, left: safeArea.leftAnchor, bottom: nil, right: safeArea.rightAnchor, paddingTop: 5, paddingLeft: 5, paddingBottom: 5, paddingRight: 5, height: 50)
     }
     
-//    func setupCollectionView() {
-//        collectionView.backgroundColor = UIColor.LightGrayBg()
-//        collectionView.delegate = self
-//        collectionView.dataSource = self
-//        collectionView.translatesAutoresizingMaskIntoConstraints = false
-//        collectionView.register(CusineCollectionViewCell.self, forCellWithReuseIdentifier: cellId)
-//        collectionView.register(EmptyCell.self, forCellWithReuseIdentifier: noCellId)
-//    }
-    
     func setupTagCollectionView() {
-        tagCollectionView.alignment = .center
-        tagCollectionView.scrollDirection = .vertical
-        tagCollectionView.delegate = self
+        cusineCollectionView.alignment = .center
+        cusineCollectionView.scrollDirection = .horizontal
+        cusineCollectionView.alignment = .fillByExpandingWidthExceptLastLine
+        cusineCollectionView.delegate = self
         let config = TTGTextTagConfig()
         config.backgroundColor = .white
         config.textColor = UIColor.orangeColor()
         config.selectedTextColor = .white
         config.selectedBackgroundColor = UIColor.orangeColor()
         
-//        tagCollectionView.addTags(["Mexican", "Italian", "Spanish", "American", "Thai", "Japanese", "Chinese", "Indian", "Cuban", "Greek", "Korean", "Cajun", "Portuguese", "Serbian", "Irish", "Peruvian", "French", "Jewish", "Swedish", "Latvian"], with: config)
-        
         firestoreDB.collection(Constants.Cusine).document(uid).getDocument { (document, error) in
             if let document = document, document.exists {
                 guard let array = document.data()?["cusine"] as? [String] else { return }
                 for name in array {
-                    self.tagCollectionView.addTags([name], with: config)
+                    self.cusineCollectionView.addTags([name], with: config)
                 }
             } else {
                 print("Document does not exist")
             }
         }
     }
-    
-//    func fetchCusines() {
-//        CusineController.shared.fetchCusineWith(uid: uid) { (result) in
-//            switch result {
-//            case true:
-//                PRId16
-//            case false:
-//                print("error in adding cusines")
-//            }
-//        }
-//    }
 
     func textTagCollectionView(_ textTagCollectionView: TTGTextTagCollectionView!, didTapTag tagText: String!, at index: UInt, selected: Bool, tagConfig config: TTGTextTagConfig!) {
         let alertController = UIAlertController(title: "Delete", message: "Would you like to delete this cusine?", preferredStyle: .alert)
@@ -112,8 +91,8 @@ class ChefSettingsViewController: UIViewController, TTGTextTagCollectionViewDele
             CusineController.shared.deleteCusineWith(uid: self.uid, type: tagText) { (result) in
                 switch result {
                 case true:
-                    self.tagCollectionView.removeTag(at: index)
-                    self.tagCollectionView.reload()
+                    self.cusineCollectionView.removeTag(at: index)
+                    self.cusineCollectionView.reload()
                 case false:
                     print("error in adding cusines")
                 }
@@ -139,9 +118,9 @@ class ChefSettingsViewController: UIViewController, TTGTextTagCollectionViewDele
         CusineController.shared.addCusineWith(uid: uid, type: text) { (result) in
             switch result {
             case true:
-                self.tagCollectionView.addTag(text, with: config)
+                self.cusineCollectionView.addTag(text, with: config)
                 self.cusineTypeTextField.text = ""
-                self.tagCollectionView.reload()
+                self.cusineCollectionView.reload()
             case false:
                 print("error in adding cusines")
             }
@@ -191,58 +170,5 @@ class ChefSettingsViewController: UIViewController, TTGTextTagCollectionViewDele
         label.textColor = .gray
         return label
     }()
-    
-//    let collectionView: UICollectionView = {
-//        let layout = UICollectionViewFlowLayout()
-//        layout.scrollDirection = .horizontal
-//        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-//        return cv
-//    }()
 }
 
-// MARK: - CollectionView
-//extension ChefSettingsViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-//
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        print(CusineController.shared.cusines.count)
-//        if CusineController.shared.cusines.count == 0 {
-//            return 1
-//        } else {
-//            return CusineController.shared.cusines.count
-//        }
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//
-//        if CusineController.shared.cusines.count == 0 {
-//            let noCell = collectionView.dequeueReusableCell(withReuseIdentifier: noCellId, for: indexPath) as! EmptyCell
-//            noCell.noPostLabel.text = "Add cusines"
-//            return noCell
-//        }
-//
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! CusineCollectionViewCell
-//        cell.cusine = CusineController.shared.cusines[indexPath.item]
-//        return cell
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//
-//        if CusineController.shared.cusines.count == 0 {
-//            return CGSize(width: view.frame.width, height: view.frame.width / 4)
-//        } else {
-//            return CGSize(width: view.frame.width / 5, height: 45)
-//        }
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-////        if cusines.count == 0 {
-////            return
-////        } else {
-////            let gallery = galleries[indexPath.row]
-////            let galleryDetail = GalleryDetailVC(collectionViewLayout: UICollectionViewFlowLayout())
-////            galleryDetail.gallery = gallery
-////            navigationController?.pushViewController(galleryDetail, animated: true)
-////        }
-//    }
-//
-//}
