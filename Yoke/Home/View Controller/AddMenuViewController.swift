474562150db89d580d3d8ea571987a3638b1b8ef
LAURA JELENICH <ljelenich85@gmail.com>
2021-01-26 15:22:46 -0500

    textfields made for add menu

--- Yoke/Home/View Controller/AddMenuViewController.swift
+++ Yoke/Home/View Controller/AddMenuViewController.swift
@@ -14,8 +14,10 @@ class AddMenuViewController: UIViewController {
     var safeArea: UILayoutGuide {
         return self.view.safeAreaLayoutGuide
     }
+    let profileImagePicker = UIImagePickerController()
     var user: User? {
         didSet {
+            
             
         }
     }
@@ -37,6 +39,7 @@ class AddMenuViewController: UIViewController {
         view.backgroundColor = .white
         view.addSubview(swipeIndicator)
         view.addSubview(menuLabel)
+        view.addSubview(menuImageView)
     }
     
     func constrainViews() {
@@ -44,6 +47,7 @@ class AddMenuViewController: UIViewController {
         swipeIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         menuLabel.anchor(top: swipeIndicator.bottomAnchor, left: nil, bottom: nil, right: nil, paddingTop: 20, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
         menuLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        menuImageView.anchor(top: menuLabel.bottomAnchor, left: safeArea.leftAnchor, bottom: nil, right: safeArea.rightAnchor, paddingTop: 20, paddingLeft: 8, paddingBottom: 0, paddingRight: 8, width: view.frame.width / 2, height: view.frame.width / 2)
     }
     
     //MARK: - Views
@@ -60,6 +64,35 @@ class AddMenuViewController: UIViewController {
         label.font = UIFont.boldSystemFont(ofSize: 15)
         label.textColor = .gray
         return label
+    }()
+    
+    let menuImageView: CustomImageView = {
+        let image = CustomImageView()
+        image.clipsToBounds = true
+        image.contentMode = .scaleAspectFill
+        image.layer.borderColor = UIColor.white.cgColor
+        image.layer.borderWidth = 2
+        image.layer.cornerRadius = 5
+        image.backgroundColor = UIColor.orangeColor()
+        return image
+    }()
+    
+    let dishNameTextField: UITextField = {
+        let text = UITextField()
+        text.placeholder = "Enter dish name"
+        return text
+    }()
+    
+    let dishDetailTextField: UITextView = {
+        let text = UITextView()
+        text.placeholder = "Enter dish details"
+        text.backgroundColor = .clear
+        text.textColor = .darkGray
+        text.isEditable = true
+        text.isScrollEnabled = true
+        text.textContainer.lineBreakMode = .byWordWrapping
+        text.font = UIFont.systemFont(ofSize: 17)
+        return text
     }()
 
 }
