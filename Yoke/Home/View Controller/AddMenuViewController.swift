808f6a0aac203cc50d985f9c0e52441093fe0ded
LAURA JELENICH <ljelenich85@gmail.com>
2021-01-26 17:09:43 -0500

    fixes to scroll view

--- Yoke/Home/View Controller/AddMenuViewController.swift
+++ Yoke/Home/View Controller/AddMenuViewController.swift
@@ -14,7 +14,7 @@ class AddMenuViewController: UIViewController {
     var safeArea: UILayoutGuide {
         return self.view.safeAreaLayoutGuide
     }
-    let profileImagePicker = UIImagePickerController()
+    let menuImagePicker = UIImagePickerController()
     var user: User? {
         didSet {
             
@@ -31,7 +31,7 @@ class AddMenuViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupImagePicker()
     }
  
     //MARK: - Helper Functions
@@ -59,8 +59,8 @@ class AddMenuViewController: UIViewController {
         menuLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         scrollView.contentSize = CGSize(width: view.frame.width, height: view.frame.height)
         scrollView.anchor(top: menuLabel.bottomAnchor, left: safeArea.leftAnchor, bottom: safeArea.bottomAnchor, right: safeArea.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
-        menuImageView.anchor(top: menuLabel.bottomAnchor, left: safeArea.leftAnchor, bottom: nil, right: safeArea.rightAnchor, paddingTop: 20, paddingLeft: 8, paddingBottom: 0, paddingRight: 8, width: view.frame.width / 2, height: view.frame.width / 2)
-        menuAddImageButton.anchor(top: menuLabel.bottomAnchor, left: safeArea.leftAnchor, bottom: nil, right: safeArea.rightAnchor, paddingTop: 20, paddingLeft: 8, paddingBottom: 0, paddingRight: 8, width: view.frame.width / 2, height: view.frame.width / 2)
+        menuImageView.anchor(top: scrollView.topAnchor, left: safeArea.leftAnchor, bottom: nil, right: safeArea.rightAnchor, paddingTop: 20, paddingLeft: 8, paddingBottom: 0, paddingRight: 8, width: view.frame.width / 2, height: 300)
+        menuAddImageButton.anchor(top: scrollView.topAnchor, left: safeArea.leftAnchor, bottom: nil, right: safeArea.rightAnchor, paddingTop: 20, paddingLeft: 8, paddingBottom: 0, paddingRight: 8, width: view.frame.width / 2, height: 300)
         dishNameTextField.anchor(top: menuImageView.bottomAnchor, left: safeArea.leftAnchor, bottom: nil, right: safeArea.rightAnchor, paddingTop: 20, paddingLeft: 8, paddingBottom: 8, paddingRight: 8, height: 45)
         dishDetailTextField.anchor(top: dishNameTextField.bottomAnchor, left: safeArea.leftAnchor, bottom: nil, right: safeArea.rightAnchor, paddingTop: 5, paddingLeft: 8, paddingBottom: 8, paddingRight: 8, height: 150)
         courseView.anchor(top: dishDetailTextField.bottomAnchor, left: safeArea.leftAnchor, bottom: nil, right: safeArea.rightAnchor, paddingTop: 5, paddingLeft: 8, paddingBottom: 8, paddingRight: 8, height: 80)
@@ -70,9 +70,27 @@ class AddMenuViewController: UIViewController {
         fixedLabel.anchor(top: fixedView.topAnchor, left: fixedView.leftAnchor, bottom: nil, right: fixedView.rightAnchor, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 8, height: 45)
         fixedSegmentedControl.anchor(top: fixedLabel.bottomAnchor, left: fixedView.leftAnchor, bottom: nil, right: fixedView.rightAnchor, paddingTop: 0, paddingLeft: 5, paddingBottom: 5, paddingRight: 5, height: 30)
     }
+    
+    func setupImagePicker() {
+        menuImagePicker.delegate = self
+    }
  
     @objc func handleAddImage() {
         print("tapped")
+        let alertVC = UIAlertController(title: "Add a Photo", message: nil, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in
+            self.menuImagePicker.dismiss(animated: true)
+        }
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { (_) in
+            self.openCamera()
+        }
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { (_) in
+            self.openPhotoLibrary()
+        }
+        alertVC.addAction(cancelAction)
+        alertVC.addAction(cameraAction)
+        alertVC.addAction(photoLibraryAction)
+        present(alertVC, animated: true)
     }
     
     @objc func handleCourseType(index: Int) {
@@ -197,7 +215,7 @@ class AddMenuViewController: UIViewController {
     }()
     
     let fixedSegmentedControl: UISegmentedControl = {
-        let seg = UISegmentedControl(items: ["Fixed Menu", "Example Menu"])
+        let seg = UISegmentedControl(items: ["Fixed Menu", "Sample Menu"])
         seg.selectedSegmentIndex = 0
         seg.backgroundColor = UIColor.orangeColor()
         seg.tintColor = UIColor.white
@@ -206,5 +224,50 @@ class AddMenuViewController: UIViewController {
         seg.addTarget(self, action: #selector(handleCourseType), for: .valueChanged)
         return seg
     }()
+}
 
+//MARK: - Image Picker
+extension AddMenuViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    func openCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            menuImagePicker.sourceType = .camera
+            menuImagePicker.allowsEditing = false
+            self.present(menuImagePicker, animated: true)
+        } else {
+            let alertVC = UIAlertController(title: "No Camera Acccess", message: "Please allow access to the camera to use this feature", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
+            alertVC.addAction(okAction)
+            self.present(alertVC, animated: true)
+        }
+    }
+
+    func openPhotoLibrary() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            menuImagePicker.sourceType = .photoLibrary
+            menuImagePicker.allowsEditing = true
+            self.present(menuImagePicker, animated: true)
+        } else {
+            let alertVC = UIAlertController(title: "No Photo Acccess", message: "Please allow access to Photos to use this feature.", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
+            alertVC.addAction(okAction)
+            self.present(alertVC, animated: true)
+        }
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[.editedImage] as? UIImage {
+            self.menuImageView.image = pickedImage
+//            UserController.shared.updateUserProfileImage(uid, profileImage: pickedImage) { (result) in
+//                switch result {
+//                case true:
+//                    print("success")
+//                case false:
+//                    print("error in uploading image")
+//                }
+//            }
+        }
+        
+        picker.dismiss(animated: true)
+    }
 }
