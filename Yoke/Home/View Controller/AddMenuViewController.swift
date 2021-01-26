e476334805679c9e5caf655b24cdc20a8db64eb2
LAURA JELENICH <ljelenich85@gmail.com>
2021-01-26 13:44:16 -0500

    addMenuVC was created with swipe indicator and nav label

--- Yoke/Home/View Controller/AddMenuViewController.swift
+++ Yoke/Home/View Controller/AddMenuViewController.swift
@@ -9,28 +9,57 @@
 import UIKit
 
 class AddMenuViewController: UIViewController {
-
+    
+    //MARK: - Properties
+    var safeArea: UILayoutGuide {
+        return self.view.safeAreaLayoutGuide
+    }
     var user: User? {
         didSet {
             
         }
     }
     
+    //MARK: - Lifecycle Methods
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setupViews()
+        constrainViews()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+ 
+    //MARK: - Helper Functions
+    func setupViews() {
+        view.backgroundColor = .white
+        view.addSubview(swipeIndicator)
+        view.addSubview(menuLabel)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func constrainViews() {
+        swipeIndicator.anchor(top: safeArea.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 20, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 100, height: 5)
+        swipeIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        menuLabel.anchor(top: swipeIndicator.bottomAnchor, left: nil, bottom: nil, right: nil, paddingTop: 20, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
+        menuLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
-    */
+    
+    //MARK: - Views
+    let swipeIndicator: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.gray
+        view.layer.cornerRadius = 5
+        return view
+    }()
+    
+    var menuLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Add a menu"
+        label.font = UIFont.boldSystemFont(ofSize: 15)
+        label.textColor = .gray
+        return label
+    }()
 
 }
