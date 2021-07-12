ac57e6a5c82a74788a7e02dd7f8184ff85ed0c28
Laura Jelenich <ljelenich85@gmail.com>
2021-07-12 16:42:33 -0400

    ...

--- Yoke/Messages/Controller/ConversationViewController.swift
+++ Yoke/Messages/Controller/ConversationViewController.swift
@@ -191,19 +191,16 @@ extension ConversationViewController: UITableViewDataSource {
         if editingStyle == .delete {
             if tableView == messageTableView {
                 let chatPartnerId = conversations[indexPath.row].message.chatPartnerId
-                tableView.beginUpdates()
-//                self.conversations.remove(at: indexPath.row)
-//                tableView.deleteRows(at: [indexPath], with: .fade)
+                self.conversations.remove(at: indexPath.row)
+                messageTableView.deleteRows(at: [indexPath], with: .left)
                 ConversationController.shared.deleteConversation(chatParnterId: chatPartnerId) { result in
                     switch result {
-                    case true:
-                        self.conversations.remove(at: indexPath.row)
-                        tableView.deleteRows(at: [indexPath], with: .fade)
-                    case false:
-                        print(Error.self)
+                    default:
+                        print("deleted")
                     }
-                tableView.endUpdates()
+                }
             }
+
         }
     }
 }
