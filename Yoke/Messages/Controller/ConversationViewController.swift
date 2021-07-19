5a46368d867477f3ce92be9534d9cbc440b2d521
Laura Jelenich <ljelenich85@gmail.com>
2021-07-19 15:44:29 -0400

    ...

--- Yoke/Messages/Controller/ConversationViewController.swift
+++ Yoke/Messages/Controller/ConversationViewController.swift
@@ -228,8 +228,10 @@ extension ConversationViewController: UITableViewDataSource {
                 ConversationController.shared.deleteConversation(chatParnterId: chatPartnerId) { result in
                     switch result {
                     default:
-                        self.conversationDictionary = [:]
-//                        self.reloadConversations()
+                        let indexToDelete = self.conversations[indexPath.row]
+                        guard let indexOfConversation = self.conversations.firstIndex(of: indexToDelete) else { return }
+                        self.conversations.remove(at: indexOfConversation)
+                        self.messageTableView.deleteRows(at: [indexPath], with: .left)
                         print("deleted")
                     }
                 }
